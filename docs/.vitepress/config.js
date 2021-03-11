d6df80baa403b2606cfec8835649225ef1a25237
daywang <daywang@tencent.com>
2021-03-11 20:29:58 +0800

    '修改vitepress配置静态地址'

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,4 @@
-const base = process.env.NODE_ENV === 'production' ? '/day-ui' : ''
+const base = process.env.NODE_ENV === 'production' ? '/day-ui-doc' : ''
 const { resolve } = require('path')
 
 module.exports = {
