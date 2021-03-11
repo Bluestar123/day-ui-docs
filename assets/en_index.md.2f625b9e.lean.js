6d22ce969b4e00b8c9b0ed94af30c46c3cfd71c1
daywang <daywang@tencent.com>
2021-03-11 19:31:16 +0800

    'day-ui-docs网页'

---
+++ assets/en_index.md.2f625b9e.lean.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,a as r}from"./app.ae69ed20.js";const a={components:{}},n='{"title":"","description":"","frontmatter":{},"relativePath":"en/index.md","lastUpdated":1615370238686}',o=r("p",null,"Write home page here.",-1);a.render=function(r,a,n,p,d,i){return e(),t("div",null,[o])};export default a;export{n as __pageData};
